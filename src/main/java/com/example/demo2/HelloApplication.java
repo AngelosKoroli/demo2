1b4bf0982d18c94110f20d498d8831eb166d44a7
angeloskoroli <akoroli25@scienceleadership.org>
2024-02-23 10:13:44 -0500

    save

--- src/main/java/com/example/demo2/HelloApplication.java
+++ src/main/java/com/example/demo2/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.demo2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View2.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 605);
         stage.setScene(scene);
         stage.show();
     }
