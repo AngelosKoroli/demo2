62b300faedcf2a6925683aff24fab10834fd02bc
brandonkay <bkay24@scienceleadership.org>
2024-02-29 08:54:12 -0500

    hi

--- src/main/java/com/example/demo2/HelloController.java
+++ src/main/java/com/example/demo2/HelloController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
 public class HelloController {
@@ -14,6 +15,7 @@ public class HelloController {
     public Button createUser;
     public Button selectFile;
     public Button bulletinBoard;
+    public ImageView imageView;
 
     public void nameEntered(){
 
