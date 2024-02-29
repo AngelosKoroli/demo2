18a03d5c443ba0ed872a0404ba5ac6b80dd88dad
angeloskoroli <akoroli25@scienceleadership.org>
2024-02-29 08:59:56 -0500

    save

--- src/main/java/com/example/demo2/State.java
+++ src/main/java/com/example/demo2/State.java
@@ -1,9 +1,30 @@
 package com.example.demo2;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 public class State implements Serializable {
     private String Ask;
     private String Answer;
 
+    public State(String Ask, String Answer) throws IOException {
+        this.Ask = Ask;
+        this.Answer = Answer;
+    }
+
+    public String getAsk() {
+        return Ask;
+    }
+
+    public void setAsk(String ask) {
+        Ask = ask;
+    }
+
+    public String getAnswer() {
+        return Answer;
+    }
+
+    public void setAnswer(String answer) {
+        Answer = answer;
+    }
 }
